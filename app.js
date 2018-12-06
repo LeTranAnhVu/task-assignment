a6e924d1b532bd39888cebc5b47040a1fb58f750
LeTranAnhVu <vule5726@gmail.com>
2018-12-06 17:51:56 +0700

    doing addtask page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
+var addTaskRouter = require('./routes/addtask');
 // Khởi tạo APP
 var app = express();
 // Gọi Template
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
+app.use('/addtask', addTaskRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
